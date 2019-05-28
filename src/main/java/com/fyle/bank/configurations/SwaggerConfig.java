ffc448b23c5938278105e897355878e03f420c17
Sudip Bhandari <sudip.post@gmail.com>
2019-05-28 09:13:09 +0530

    Added swagger meta-data

--- src/main/java/com/fyle/bank/configurations/SwaggerConfig.java
+++ src/main/java/com/fyle/bank/configurations/SwaggerConfig.java
@@ -1,23 +1,48 @@
 package com.fyle.bank.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Autowired
+    Environment environment;
+
+    private ApiInfo metaData() {
+        String serviceName="Bank Service running at profile:: "+ Arrays.toString(environment.getActiveProfiles());
+        return new ApiInfo(
+                serviceName,
+                "This is a demo of bank and branch services",
+                "1.0.0",
+                "Feel free to play around",
+                 new Contact("Sudip Bhandari","https://sudipbhandari126.github.io","sudip.post@gmail.com"),
+                null,
+                null,
+                new ArrayList<>());
+    }
+
+
     @Bean
     public Docket api() {
         return new Docket(SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaData());
     }
 }
